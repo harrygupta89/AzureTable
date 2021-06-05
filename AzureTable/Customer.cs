5f82c5ee1e314f73db09026eb920493dec1fabb3
harrygupta89 <harrysist@gmail.com>
2021-06-05 08:54:03 +0530

    add Entity to the table.

---
+++ AzureTable/Customer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Azure.Cosmos.Table;
+
+
+namespace AzureTable
+{
+    public class Customer : TableEntity
+    {
+        public string customername { get; set; }
+
+        public Customer(string _customername, string _city, string _customerid)
+        {
+            PartitionKey = _city;
+            RowKey = _customerid;
+            customername = _customername;
+        }
+    }
+}
